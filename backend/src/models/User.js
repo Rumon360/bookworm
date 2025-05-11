eeb471b7ae2e56bfe5c2d145894ba0730633ce3a
Rumon360 <hmk.rumon@gmail.com>
2025-05-11 21:41:18 +0600

    chore: add timestamp to user model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,42 +3,45 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import { validateEmail } from "../validators/index.js";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-    minlength: 3,
-    maxlength: 30,
-    match: [
-      /^[a-zA-Z0-9_]+$/,
-      "Username must contain only letters, numbers, and underscores",
-    ],
-    lowercase: true, // force lowercase to avoid case-sensitive duplicates
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 30,
+      match: [
+        /^[a-zA-Z0-9_]+$/,
+        "Username must contain only letters, numbers, and underscores",
+      ],
+      lowercase: true, // force lowercase to avoid case-sensitive duplicates
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    profileImage: {
+      type: String,
+      default: "https://placehold.co/98x98",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    validate: [validateEmail, "Please fill a valid email address"],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
-    ],
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-  profileImage: {
-    type: String,
-    default: "https://placehold.co/98x98",
-  },
-});
+  { timestamps: true }
+);
 
 // hash password before saving to database
 userSchema.pre("save", async function (next) {
