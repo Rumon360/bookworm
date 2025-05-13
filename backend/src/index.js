f8f6edfb92eb2684bc41d460bbe53420ce37e60d
Rumon360 <hmk.rumon@gmail.com>
2025-05-14 01:07:12 +0600

    feat: add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
 // routes
@@ -12,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/book", bookRoutes);
