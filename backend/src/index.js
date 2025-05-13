b109bea036effb74b5dbd90687dd7dc13ff24632
Rumon360 <hmk.rumon@gmail.com>
2025-05-14 01:16:11 +0600

    feat: global rate limit

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,11 +9,15 @@ import bookRoutes from "./routes/bookRoutes.js";
 // db
 import { connectDB } from "./lib/db.js";
 
+// rate limiter
+import { globalRateLimiter } from "./lib/ratelimit.js";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(globalRateLimiter);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/book", bookRoutes);
--- backend/src/lib/ratelimit.js
+++ backend/src/lib/ratelimit.js
@@ -1,5 +1,13 @@
 import rateLimit from "express-rate-limit";
 
+const globalRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again after 15 minutes",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 const authRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 50,
@@ -14,4 +22,4 @@ const bookRateLimiter = rateLimit({
   headers: true,
 });
 
-export { authRateLimiter, bookRateLimiter };
+export { authRateLimiter, bookRateLimiter, globalRateLimiter };
