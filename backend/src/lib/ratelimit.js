fcae698fda49577ec37ac7194fcf819e303ac5b4
Rumon360 <hmk.rumon@gmail.com>
2025-05-14 01:16:39 +0600

    chore: better msg

--- backend/src/lib/ratelimit.js
+++ backend/src/lib/ratelimit.js
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 const globalRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
-  message: "Too many requests from this IP, please try again after 15 minutes",
+  message: "Too many requests, please try again after 15 minutes",
   standardHeaders: true,
   legacyHeaders: false,
 });
